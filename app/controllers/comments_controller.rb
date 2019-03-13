910ea1451e2b4eab712bc0269b90d8f3a79e7973
Ifeanyi Osinakayah Joseph <ifeanyiosinakayah15@gmail.com>
2019-03-13 15:13:17 +0100

    Created links more links

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,28 @@
 class CommentsController < ApplicationController
   include CommentsHelper
+
+  def edit
+    @comment = Comment.find_by(id: params[:id])
+  end
+
+  def update
+    @comment = Comment.find_by(id: params[:id])
+    if @comment.update(comment_params)
+      redirect_to comments_post_path(@comment.post_id)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @comment = Comment.find_by(id: params[:id])
+    if @comment.destroy
+      redirect_to comments_post_path(@comment.post_id)
+    else
+      redirect_to comments_post_path(@comment.post_id)
+    end
+  end
+
   def create
     @comment = current_user.comments.build(comment_params)
     @comment.post_id = params[:id]
