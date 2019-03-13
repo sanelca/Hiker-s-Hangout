e787fe4d72048b4315aa89c54e498fcff0afe922
Sanel Hodzic <sanel_hodzic_tz_91@hotmail.com>
2019-03-13 12:57:48 +0100

    home registration page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-  root to: "home#index"
+  root "home#index"
   resources :users, only: [:index, :show, :edit]
   resources :friends
   resources :comments
