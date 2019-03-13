001ad997e592168a6edc2f6ca4fd99a9b9793474
Ifeanyi Osinakayah Joseph <ifeanyiosinakayah15@gmail.com>
2019-03-13 15:31:15 +0100

    USer controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+  include FriendsHelper
+
+  def index
+    @users = User.where.not(id: current_user.id)
+    @invited = Friend.where('user_id=? OR friend_id=?', current_user.id, current_user.id)
+  end
+  
 end
