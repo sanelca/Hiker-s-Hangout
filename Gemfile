dbc68ef50a62c84ab6c190a46cdbee94276e9756
Sanel Hodzic <sanel_hodzic_tz_91@hotmail.com>
2019-06-18 05:18:09 +0200

    Tests model use let

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.4.2'
+ruby '2.5.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.2'
@@ -27,6 +27,7 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 gem 'rails-controller-testing'
+gem 'rubocop-rails'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 gem 'devise'
@@ -37,6 +38,7 @@ gem "awesome_print", require:"ap"
 gem 'bootsnap', '>= 1.1.0', require: false
 
 group :development, :test do
+  gem 'rspec_boolean'
   gem 'rspec-rails', '~> 3.8'
   gem 'factory_bot_rails'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,23 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+  let(:user) { User.create first_name: 'Sanel', last_name: 'Hodzic', email: 'sancho@test.com', password: 'istanbul', password_confirmation: 'istanbul' }
+  let(:user1) { User.create first_name: 'Sanel1', last_name: 'Hodzic1', email: 'sancho1@test.com', password: 'istanbul', password_confirmation: 'istanbul' }
+
+  let(:post1) { Post.create content: "sdkjashg dshdkasg", user_id: user.id }
+
+  let(:comment) { Comment.new(content: "Testing comments", post_id: post1.id, user_id: user.id).save }
+  let(:comment1) { Comment.new(content: "Testing comments", post_id: "", user_id: user.id).save }
+  let(:comment2) { Comment.new(content: "Testing comments", post_id: post1.id, user_id: "").save }
+
   context "Testing comments" do
     it "With all data" do
-      user = User.new(first_name: "Sanel", last_name: "Hodz", email: "sancho@test.com", password: "istanbul", password_confirmation: "istanbul" ).save
-      post = Post.new(content: "sdkjashg dshdkasg", user_id: 1).save
-      comment = Comment.new(content: "Testing comments", post_id: 1, user_id: 1).save
       expect(comment).to eq(true)
     end
-    it "Without user id" do
-      user = User.new(first_name: "Sanel", last_name: "Hodz", email: "sancho@test.com", password: "istanbul", password_confirmation: "istanbul" ).save
-      post = Post.new(content: "sdkjashg dshdkasg", user_id: 1).save
-      comment = Comment.new(content: "Testing comments", post_id: 1).save
-      expect(comment).to eq(false)
-    end
+
     it "Without post id" do
-      user = User.new(first_name: "Sanel", last_name: "Hodz", email: "sancho@test.com", password: "istanbul", password_confirmation: "istanbul" ).save
-      comment = Comment.new(content: "Testing comments", user_id: 1).save
-      expect(comment).to eq(false)
+      expect(comment1).to eq(false)
+    end
+
+    it "Without user id" do
+      expect(comment2).to eq(false)
     end
   end
 
