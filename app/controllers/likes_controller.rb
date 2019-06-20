037be9a3c4519602d8bc23924d27c17c2a3670e5
Sanel Hodzic <sanel_hodzic_tz_91@hotmail.com>
2019-06-20 13:11:15 +0200

    Like unlike modify

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,20 @@
 class LikesController < ApplicationController
+  include LikesHelper
+  def create
+    @like = Like.new
+    @like.likeable_type = params[:type]
+    @like.likeable_id = params[:likeable_id]
+    @like.user_id = current_user.id
+    if @like.save
+      redirect_to(redirectByLikeableType(@like.likeable_type, @like.likeable_id))
+    end
+  end
+
+  def destroy
+    @like = Like.find_by(id: params[:id])
+    if @like.user_id == current_user.id
+      @like.destroy
+      redirect_to(redirectByLikeableType(@like.likeable_type, @like.likeable_id))
+    end
+  end
 end
